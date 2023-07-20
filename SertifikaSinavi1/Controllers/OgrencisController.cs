3d707d10d4b7611b20b9e27e6f9020248d484d8b
Mustafa Kerem Tay <81324994+keremmtay@users.noreply.github.com>
2023-07-20 22:08:22 +0300

    Add project files.

---
+++ SertifikaSinavi1/Controllers/OgrencisController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SertifikaSinavi1.Models;
+using System.Net;
+
+namespace SertifikaSinavi1.Controllers
+{
+    public class OgrencisController : Controller
+    {
+        private Model1 db = new Model1();
+
+        public ActionResult Index()
+        {
+            return View(db.MyEntities.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Ogrenci ogrenci = db.MyEntities.Find(id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+            return View(ogrenci);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Ogrenci ogrenci)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MyEntities.Add(ogrenci);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ogrenci);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Ogrenci ogrenci = db.MyEntities.Find(id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+            return View(ogrenci);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Ogrenci ogrenci)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ogrenci).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ogrenci);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Ogrenci ogrenci = db.MyEntities.Find(id);
+            if (ogrenci == null)
+            {
+                return NotFound();
+            }
+            return View(ogrenci);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ogrenci ogrenci = db.MyEntities.Find(id);
+            db.MyEntities.Remove(ogrenci);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ SertifikaSinavi1/Models/Model1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SertifikaSinavi1.Models
+{
+    public class Model1 : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ASUS;Database=SertifikaSinavi1;Integrated Security=true");
+        }
+
+        public virtual DbSet<Ogrenci> MyEntities { get; set; }
+
+    }
+}
